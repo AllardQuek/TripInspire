e538253976a600de878ea2576ce611256bd13943
DESKTOP-O32GPF5\steph <stephaniechenshiyao@gmail.com>
2023-03-31 18:43:41 +0800

    Add questions to form

--- pages/form.js
+++ pages/form.js
@@ -11,17 +11,16 @@ import {
   FormGroup,
   Checkbox,
   Button,
+  Slider
 } from "@mui/material";
 import { Grid } from "@mui/material";
 import { signOut } from "next-auth/react";
 
 const Form = ({ name }) => {
   const initialValues = {
-    firstName: "",
-    lastName: "",
-    gender: "male",
     country: "Canada",
-    hobby: "",
+    pace: "",
+    transport: "",
   };
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -41,60 +40,11 @@ const Form = ({ name }) => {
     <>
       <form onSubmit={handleSubmit}>
         <Grid container alignItems="center" justify="center" direction="column">
-          <h1>What is your ideal trip, {name}?</h1>
-          <Grid item>
-            <TextField
-              id="firstName"
-              name="firstName"
-              label="First name"
-              type="text"
-              value={formValues.firstName}
-              onChange={handleInputChange}
-            />
-          </Grid>
-          <Grid item>
-            <TextField
-              id="lastName"
-              name="lastName"
-              label="Last name"
-              type="text"
-              value={formValues.lastName}
-              onChange={handleInputChange}
-            />
-          </Grid>
-          <Grid item>
-            <FormControl>
-              <FormLabel>Gender</FormLabel>
-              <RadioGroup
-                name="gender"
-                value={formValues.gender}
-                onChange={handleInputChange}
-                row
-              >
-                <FormControlLabel
-                  key="male"
-                  value="male"
-                  control={<Radio size="small" />}
-                  label="Male"
-                />
-                <FormControlLabel
-                  key="female"
-                  value="female"
-                  control={<Radio size="small" />}
-                  label="Female"
-                />
-                <FormControlLabel
-                  key="other"
-                  value="other"
-                  control={<Radio size="small" />}
-                  label="Other"
-                />
-              </RadioGroup>
-            </FormControl>
-          </Grid>
+          <h1>Tell me more about your ideal trip, {name}.</h1>
 
           <Grid item>
             <FormControl>
+              <FormLabel>Country</FormLabel>
               <Select
                 name="country"
                 value={formValues.country}
@@ -123,26 +73,175 @@ const Form = ({ name }) => {
           </Grid>
 
           <Grid item>
-            <FormLabel>Hobby</FormLabel>
-            <FormGroup>
+            <FormControl>
+              <FormLabel>How important is each of the following to you on a trip?</FormLabel>
+              <FormLabel>Food</FormLabel>
+              <Slider
+                aria-label="food"
+                defaultValue={5}
+                valueLabelDisplay="auto"
+                step={1}
+                marks
+                min={1}
+                max={10}
+                onChange={handleInputChange}
+              />
+              <FormLabel>Adventure</FormLabel>
+              <Slider
+                aria-label="adventure"
+                defaultValue={5}
+                valueLabelDisplay="auto"
+                step={1}
+                marks
+                min={1}
+                max={10}
+                onChange={handleInputChange}
+              />
+              <FormLabel>Nature</FormLabel>
+              <Slider
+                aria-label="nature"
+                defaultValue={5}
+                valueLabelDisplay="auto"
+                step={1}
+                marks
+                min={1}
+                max={10}
+                onChange={handleInputChange}
+              />
+              <FormLabel>Culture</FormLabel>
+              <Slider
+                aria-label="culture"
+                defaultValue={5}
+                valueLabelDisplay="auto"
+                step={1}
+                marks
+                min={1}
+                max={10}
+                onChange={handleInputChange}
+              />
+              <FormLabel>City</FormLabel>
+              <Slider
+                aria-label="city"
+                defaultValue={5}
+                valueLabelDisplay="auto"
+                step={1}
+                marks
+                min={1}
+                max={10}
+                onChange={handleInputChange}
+              />
+              <FormLabel>Shopping</FormLabel>
+              <Slider
+                aria-label="shopping"
+                defaultValue={5}
+                valueLabelDisplay="auto"
+                step={1}
+                marks
+                min={1}
+                max={10}
+                onChange={handleInputChange}
+              />
+              <FormLabel>Entertainment</FormLabel>
+              <Slider
+                aria-label="entertainment"
+                defaultValue={5}
+                valueLabelDisplay="auto"
+                step={1}
+                marks
+                min={1}
+                max={10}
+                onChange={handleInputChange}
+              />
+            </FormControl>
+          </Grid>
+
+          <Grid item>
+            <FormControl>
+              <FormLabel>What is your budget for the trip?</FormLabel>
+              <Slider
+                aria-label="budget"
+                defaultValue={5}
+                valueLabelDisplay="auto"
+                step={1}
+                marks
+                min={1}
+                max={10}
+                onChange={handleInputChange}
+              />
+            </FormControl>
+          </Grid>
+
+          <Grid item>
+            <FormControl>
+              <FormLabel>What is your preferred pace of trip?</FormLabel>
+              <RadioGroup
+                name="pace"
+                value={formValues.pace}
+                onChange={handleInputChange}
+              >
+                <FormControlLabel
+                  key="packed"
+                  value="packed"
+                  control={<Radio size="small" />}
+                  label="Packed (>= 5 attractions a day)"
+                />
+                <FormControlLabel
+                  key="comfortable"
+                  value="comfortable"
+                  control={<Radio size="small" />}
+                  label="Comfortable (3-4 attractions a day)"
+                />
+                <FormControlLabel
+                  key="chill"
+                  value="chill"
+                  control={<Radio size="small" />}
+                  label="Chill (1-2 attractions a day)"
+                />
+              </RadioGroup>
+            </FormControl>
+          </Grid>
+
+          <Grid item>
+            <FormControl>
+              <FormLabel>How much local food do you want to eat?</FormLabel>
+              <Slider
+                aria-label="food"
+                defaultValue={5}
+                valueLabelDisplay="auto"
+                step={1}
+                marks
+                min={0}
+                max={10}
+                onChange={handleInputChange}
+              />
+            </FormControl>
+          </Grid>
+
+          <Grid item>
+            <FormLabel>What are your preferred modes of transport?</FormLabel>
+            <FormGroup
+              value={formValues.pace}
+              onChange={handleInputChange}
+            >
               <FormControlLabel
-                control={<Checkbox name="hobby" defaultChecked />}
-                label="Writing"
+                control={<Checkbox name="transport" defaultChecked />}
+                label="Walking"
               />
               <FormControlLabel
-                control={<Checkbox name="hobby" />}
-                label="Dance"
+                control={<Checkbox name="transport" />}
+                label="Driving"
               />
               <FormControlLabel
-                control={<Checkbox name="hobby" />}
-                label="Painting"
+                control={<Checkbox name="transport" />}
+                label="Public transport"
               />
               <FormControlLabel
-                control={<Checkbox name="hobby" />}
-                label="Video Game"
+                control={<Checkbox name="transport" />}
+                label="Ride-hailing"
               />
             </FormGroup>
           </Grid>
+
           <Grid item>
             <Button
               variant="contained"
