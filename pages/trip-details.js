3b4b8e1712eac47301bbd729f8bc0c76338b2e4b
Allard <AllardQuek@users.noreply.github.com>
2023-04-08 11:48:56 +0800

    Rename route to trip-details

--- pages/trip-details.js
+++ pages/trip-details.js
@@ -6,7 +6,6 @@ import Select, { SelectChangeEvent } from "@mui/material/Select";
 import { useState } from "react";
 import NavBar from "../components/NavBar";
 
-
 export default function TripDetails() {
   const [destination, setDestination] = useState("");
   const [numDays, setNumDays] = useState("");
@@ -15,79 +14,76 @@ export default function TripDetails() {
 
   const handleDestinationChange = (event) => {
     setDestination(event.target.value);
-    console.log(event.target.value);
   };
 
   const handleDaysChange = (event) => {
     setNumDays(event.target.value);
-    console.log(event.target.value);
   };
 
   const handleNightsChange = (event) => {
     setNumNights(event.target.value);
-    console.log(event.target.value);
   };
 
   return (
     <div>
-    <NavBar />
-        <Section>
+      <NavBar />
+      <Section>
         <Grid
-            className="trip-details"
-            container
-            spacing={2}
-            alignItems="center"
-            justifyContent="center"
+          className="trip-details"
+          container
+          spacing={2}
+          alignItems="center"
+          justifyContent="center"
         >
-            <Grid item xs={4}>
+          <Grid item xs={4}>
             <FormControl fullWidth>
-                <InputLabel id="destination-label">Destination</InputLabel>
-                <Select
+              <InputLabel id="destination-label">Destination</InputLabel>
+              <Select
                 value={destination}
                 label="destination"
                 onChange={handleDestinationChange}
-                >
+              >
                 <MenuItem value="Singapore">Singapore</MenuItem>
                 <MenuItem value="Munich">Munich</MenuItem>
                 <MenuItem value="Vancouver">Vancouver</MenuItem>
-                </Select>
+              </Select>
             </FormControl>
-            </Grid>
-            <Grid item xs={2}>
+          </Grid>
+          <Grid item xs={2}>
             <FormControl fullWidth>
-                <InputLabel id="num-days-label">Days</InputLabel>
-                <Select value={numDays} label="Days" onChange={handleDaysChange}>
+              <InputLabel id="num-days-label">Days</InputLabel>
+              <Select value={numDays} label="Days" onChange={handleDaysChange}>
                 {numRange.map((item, index) => (
-                    <MenuItem key={index} value={item}>
+                  <MenuItem key={index} value={item}>
                     {item}
-                    </MenuItem>
+                  </MenuItem>
                 ))}
-                </Select>
+              </Select>
             </FormControl>
-            </Grid>
-            <Grid item xs={2}>
+          </Grid>
+          <Grid item xs={2}>
             <FormControl fullWidth>
-                <InputLabel id="num-nights-label">Nights</InputLabel>
-                <Select
+              <InputLabel id="num-nights-label">Nights</InputLabel>
+              <Select
                 value={numNights}
                 label="Nights"
                 onChange={handleNightsChange}
-                >
+              >
                 {numRange.map((item, index) => (
-                    <MenuItem key={index} value={item}>
+                  <MenuItem key={index} value={item}>
                     {item}
-                    </MenuItem>
+                  </MenuItem>
                 ))}
-                </Select>
+              </Select>
             </FormControl>
-            </Grid>
-            <Grid item xs={2}>
+          </Grid>
+          <Grid item xs={2}>
             <Button variant="contained" color="secondary">
-                Explore Now
+              Explore Now
             </Button>
-            </Grid>
+          </Grid>
         </Grid>
-        </Section>
+      </Section>
     </div>
   );
 }
