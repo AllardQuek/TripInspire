31893891b9cd5c973adef85c71799645132f56bf
Allard <AllardQuek@users.noreply.github.com>
2023-04-09 11:49:43 +0800

    Create format for table results

--- pages/places.js
+++ pages/places.js
@@ -4,25 +4,7 @@ import useSWR from "swr";
 import { CompactTable } from "@table-library/react-table-library/compact";
 import { useTheme } from "@table-library/react-table-library/theme";
 import { getTheme } from "@table-library/react-table-library/baseline";
-
-const nodes = [
-  {
-    id: "0",
-    name: "Shopping List",
-    deadline: new Date(2020, 1, 15),
-    type: "TASK",
-    isComplete: true,
-    nodes: 3,
-  },
-  {
-    id: "1",
-    name: "Grocery List",
-    deadline: new Date(2020, 1, 15),
-    type: "TASK",
-    isComplete: true,
-    nodes: 3,
-  },
-];
+// import attraction_data from "../data/attractions.json";
 
 // Write a fetcher function to wrap the native fetch function and return the result of a call to url in json format
 const fetcher = (url) => fetch(url).then((res) => res.json());
@@ -30,29 +12,25 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 export default function Places() {
   // Set up SWR to run the fetcher function when calling "/api/staticdata"
   // There are 3 possible states: (1) loading when data is null (2) ready when the data is returned (3) error when there was an error fetching the data
-  const nodes_data = { nodes };
   const theme = useTheme(getTheme());
+  const numDays = 3;
 
   const { data, error } = useSWR("/api/staticdata", fetcher);
   let attractions = {};
 
   const COLUMNS = [
-    { label: "Task", renderCell: (item) => item.name },
+    { label: "Name", renderCell: (item) => item.name, resize: true },
     {
-      label: "Deadline",
-      renderCell: (item) =>
-        item.deadline.toLocaleDateString("en-US", {
-          year: "numeric",
-          month: "2-digit",
-          day: "2-digit",
-        }),
+      label: "Address",
+      renderCell: (item) => item.address,
+      resize: true,
     },
-    { label: "Type", renderCell: (item) => item.type },
+    { label: "Budget", renderCell: (item) => item.budget, resize: true },
     {
-      label: "Complete",
-      renderCell: (item) => item.isComplete.toString(),
+      label: "Category",
+      renderCell: (item) => item.category,
+      resize: true,
     },
-    { label: "Tasks", renderCell: (item) => item.nodes },
   ];
 
   // Convert data to  JSON format if it is not already
@@ -61,31 +39,40 @@ export default function Places() {
   } catch (err) {
     console.log("Error: ", err.message);
   }
+  console.log(attractions);
+  const city_attractions = { nodes: attractions["Singapore"] };
 
   // Handle the error state
   if (error) return <div>Failed to load</div>;
   // Handle the loading state
   if (!data) return <div>Loading...</div>;
   // Handle the ready state and display the result contained in the data object mapped to the structure of the json file
+
   return (
     <div>
-      <h1>Attractions</h1>
+      <h1>Your Itinerary</h1>
       {attractions && (
-        <ul>
-          {attractions.Singapore.map((attraction) => (
-            <li key={attraction.id}>
-              {attraction.name}
-              <ul>
-                <li>{attraction.address}</li>
-                <li>Budget: {attraction.budget}</li>
-                <li>Pace: {attraction.pace}</li>
-                <li>Accessibility: {attraction.accessibility}</li>
-              </ul>
-            </li>
-          ))}
-        </ul>
+        <div>
+          {/* <ul>
+            {attractions.Singapore.map((attraction) => (
+              <li key={attraction.id}>
+                {attraction.name}
+                <ul>
+                  <li>{attraction.address}</li>
+                  <li>Budget: {attraction.budget}</li>
+                  <li>Pace: {attraction.pace}</li>
+                  <li>Accessibility: {attraction.accessibility}</li>
+                </ul>
+              </li>
+            ))}
+          </ul> */}
+          <CompactTable
+            theme={theme}
+            columns={COLUMNS}
+            data={city_attractions}
+          />
+        </div>
       )}
-      <CompactTable theme={theme} columns={COLUMNS} data={nodes_data} />;
     </div>
   );
 }
